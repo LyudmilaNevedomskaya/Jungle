b523ee8b44605ca60a41a260302a70ddaea6b396
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-09-16 20:48:56 +0000

    Feature: User Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,11 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
   def authorize
     redirect_to '/login' unless current_user
   end
