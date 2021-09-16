f81c67803970733b1d902a1d1ed1b70b17091218
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-09-16 01:30:48 +0000

    Feature: Admin Dashboard - Counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   before_filter :authenticate
   
   def show
+    @product_count = Product.count
+    @category_list = Product.group(:category_id).count
   end
 end
